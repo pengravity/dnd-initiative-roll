f4700e061cef16752bf67cc132715e006d4042db
Andy <alipinski@mail.com>
2022-10-25 12:20:19 +0200

    initial setup

---
+++ src/Data.js
@@ -0,0 +1,46 @@
+const Data = [
+  {
+    id: 1,
+    name: 'player1',
+    init: 5,
+    dexMod: 2,
+  },
+  {
+    id: 2,
+    name: 'player2',
+    init: 7,
+    dexMod: 3,
+  },
+  {
+    id: 3,
+    name: 'player3',
+    init: 3,
+    dexMod: 0,
+  },
+  {
+    id: 4,
+    name: 'player4',
+    init: 15,
+    dexMod: 0,
+  },
+  {
+    id: 5,
+    name: 'Monster1',
+    init: 7,
+    dexMod: 0,
+  },
+  {
+    id: 6,
+    name: 'Monster2',
+    init: 11,
+    dexMod: 0,
+  },
+  {
+    id: 7,
+    name: 'Monster3',
+    init: 13,
+    dexMod: 0,
+  },
+];
+
+export default Data;
